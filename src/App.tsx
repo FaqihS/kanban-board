e3811d0f0b29ff8aea082493153c38fb1bd63e12
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2023-11-25 21:49:26 +0800

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import { TaskCard } from "./components/TaskCard";
-import { tasks, statuses } from "./utils/data-task";
+import { tasks as initialTask, statuses, Task } from "./utils/data-task";
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>(initialTask);
   const column = statuses.map((stat) => {
     const tasksInColumn = tasks.filter((task) => task.status === stat);
-    console.log(tasksInColumn)
     return {
       title: stat,
       tasks: tasksInColumn,
     };
   });
 
+  const updateTask = (task: Task) => {
+    const newTasks = tasks.map((t) =>
+      t.id === task.id ? { ...task } : { ...t }
+    );
+    setTasks(newTasks);
+  };
+
   return (
     <div className="flex divide-x">
       {column.map((c) => (
         <div>
-          <h2 className="text-3xl p-2 capitalize font-bold text-gray-500">{c.title}</h2>
+          <h2 className="text-3xl p-2 capitalize font-bold text-gray-500">
+            {c.title}
+          </h2>
           {c.tasks.map((t) => (
-            <TaskCard task={t} />
+            <TaskCard task={t} updateTask={updateTask} />
           ))}
         </div>
       ))}
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import { Task } from "../utils/data-task";
 
-export function TaskCard({task}:{
-  task: Task
+export function TaskCard({
+  task,
+  updateTask,
+}: {
+  task: Task;
+  updateTask: (task: Task) => void;
 }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <div className="bg-gray-50 border rounded-lg px-2 m-2">
-      <div className="text-base font-base py-2">{task.title}</div>
+      <div className="text-base font-base py-2"></div>
+      {isEditing ? (
+        <input
+          type="text"
+          className="w-full"
+          onBlur={() => setIsEditing(false)}
+          value={task.title}
+          onChange={(e)=>updateTask({...task,title: e.target.value})}
+        />
+      ) : (
+        <div className="" onClick={() => setIsEditing(true)}>
+          {task.title}
+        </div>
+      )}
       <div className="flex gap-4 justify-between text-gray-500 text-sm py-2">
         <div>{task.id}</div>
         <div>{task.point}</div>
