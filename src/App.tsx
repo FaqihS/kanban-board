2a8e75c04b0b3187ce49340a921b210de8769f1d
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2023-11-25 22:20:24 +0800

    hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import { TaskCard } from "./components/TaskCard";
-import { tasks as initialTask, Status, statuses, Task } from "./utils/data-task";
+import {
+  tasks as initialTask,
+  Status,
+  statuses,
+  Task,
+} from "./utils/data-task";
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>(initialTask);
   const column = statuses.map((stat) => {
     const tasksInColumn = tasks.filter((task) => task.status === stat);
     return {
-      title: stat,
+      status: stat,
       tasks: tasksInColumn,
     };
   });
@@ -21,23 +26,36 @@ function App() {
   };
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>, status: Status) => {
-    e.preventDefault()
-    const id = e.dataTransfer.getData("id")
-    const task = tasks.find(t=>t.id === id)
-    task && updateTask({...task,status})
+    e.preventDefault();
+    const id = e.dataTransfer.getData("id");
+    const task = tasks.find((t) => t.id === id);
+    task && updateTask({ ...task, status });
+    setCurrentHover(null)
+  };
 
-  }
+  const [currentHover, setCurrentHover] = useState<Status | null>(null);
+  const handleDragEnter = (status: Status) => {
+    setCurrentHover(status);
+  };
 
   return (
-    <div className="flex divide-x">
+    <div className="flex divide-x ">
       {column.map((c) => (
-        <div onDrop={(e)=> handleDrop(e,c.title)} onDragOver={(e)=>e.preventDefault()}>
+        <div
+          onDrop={(e) => handleDrop(e, c.status)}
+          onDragOver={(e) => e.preventDefault()}
+          onDragEnter={() => handleDragEnter(c.status)}
+        >
           <h2 className="text-3xl p-2 capitalize font-bold text-gray-500">
-            {c.title}
+            {c.status}
           </h2>
+          <div className={currentHover === c.status ? 'bg-gray-700 h-full' : ''}>
+
           {c.tasks.map((t) => (
+            
             <TaskCard task={t} updateTask={updateTask} />
           ))}
+          </div>
         </div>
       ))}
     </div>
