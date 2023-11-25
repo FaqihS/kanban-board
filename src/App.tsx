b735dde9e958af1ff69df1752d63ad51db49ad7e
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2023-11-25 19:32:26 +0800

    first card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
 
 function App() {
+  const title = "Do Search"
+  const id = "BUS-15"
+  const point = "1"
 
   return (
-    <div className='text-xl text-red-400'>
-      Hello
+    <div className='bg-gray-50 border rounded-lg px-2 m-2'>
+      <div className='text-base font-semibold py-2'>
+        {title}
+      </div>
+      <div className='flex gap-4 justify-between text-gray-700 py-2'>
+        <div>{id}</div>
+        <div>{point}</div>
+      </div>
     </div>
   )
 }
